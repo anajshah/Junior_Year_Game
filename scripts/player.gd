fc6e18cd4ef37cadc05d41bf382482cddb54267a
Aariana <anajshah@gmail.com>
2026-01-20 08:41:02 -0800

    hour 1 code vanished i swear i comitted that. anywho my first hour didn't save so i redid it and am committing agani for my hour two commit fingers crossed

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,8 +18,22 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+		
+	elif direction <0:
+		animated_sprite_2d.flip_h = true
+		
+	if direction == 0:
+		animated_sprite_2d.play("idle")
+	else:
+		animated_sprite_2d.play("walk")
+	
+	
 	if direction:
 		velocity.x = direction * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
