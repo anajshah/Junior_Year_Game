e851e715bca556f20eda6b65aa2203225b79a0b8
anajshah <anajshah@gmail.com>
2026-01-22 22:57:49 -0800

    hour 10 trying to create like a lag sort of thing where it stpos to turn

--- scripts/gatsbys_car.gd
+++ scripts/gatsbys_car.gd
@@ -1,32 +1,60 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
+const SPEED = 400.0
 @onready var timer: Timer = $Timer
 
 var player_pos: float
 var other_pos: float
 var player: CharacterBody2D
+var direction = -1
+
 
 func _ready():
 	var scene_root = get_tree().current_scene
 	player = scene_root.get_node_or_null("Player")
 
-func _physics_process(delta: float) -> void:
+
+func _physics_process(delta: float) -> void: 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	var direction = 0
 	player_pos = global_position.x
 	other_pos = player.global_position.x
+	print(other_pos-player_pos)
+	if abs(other_pos - player_pos) <= 1:
+		timer.start()
 	
-	if (other_pos - player_pos)<0:
-		direction = -1
-		print("move left")
-	elif (other_pos - player_pos)>0:
-		direction = 1
-		print("move right")
-		
-	
+	#if (other_pos - player_pos)<0:
+		#direction = -1
+		#print("move left")
+	#elif(other_pos - player_pos)>0:
+		#direction = 1
+		#print("move right")
+		#
 	
 	velocity.x = direction * SPEED
 
 	move_and_slide()
+	
+
+func _on_timer_timeout() -> void:
+	direction = direction * -1
+	#if (other_pos - player_pos)<0:
+		#direction = -1
+		#print("move left")
+	#elif(other_pos - player_pos)>0:
+		#direction = 1
+		#print("move right")
+
+
+
+#
+#func _on_timer_timeout():	
+	#var direction = 0
+	#if (other_pos - player_pos)<0:
+		#direction = -1
+		#print("move left")
+	#elif (other_pos - player_pos)>0:
+		#direction = 1
+		#print("move right")
+	#
+	#velocity.x = direction * SPEED
