550d7f1f447c4da84eadcd375c091cff9bc714a9
anajshah <anajshah@gmail.com>
2026-01-24 14:10:47 -0800

    just debugged the heck out of evil integral and also the doors are not opening like what the heck but the evil integral now only runs once. have to figure out calling  from period 2 final boss but that should be fine.

--- scripts/cupboard_openable.gd
+++ scripts/cupboard_openable.gd
@@ -1,33 +1,56 @@
 extends Node2D
 
 @onready var area_2d: Area2D = $Area2D
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var collision_shape_2d_2: CollisionShape2D = $Area2D/CollisionShape2D2
+@onready var collision_shape_2d_3: CollisionShape2D = $Area2D/CollisionShape2D3
+@onready var collision_shape_2d_4: CollisionShape2D = $Area2D/CollisionShape2D4
+@onready var collision_shape_2d_5: CollisionShape2D = $Area2D/CollisionShape2D5
+@onready var collision_shape_2d_6: CollisionShape2D = $Area2D/CollisionShape2D6
+
+@onready var shape_map := {
+	0: collision_shape_2d,
+	1: collision_shape_2d_2,
+	2: collision_shape_2d_3,
+	3: collision_shape_2d_4,
+	4: collision_shape_2d_5,
+	5: collision_shape_2d_6
+}
 
 var player_in_range := false
 var is_open := false
 
 
+func _on_area_2d_body_shape_entered(
+	body_rid: RID,
+	body: Node,
+	body_shape_index: int,
+	local_shape_index: int
+) -> void:
+	if body.name == "Player":
+		print("Player touched:", shape_map[local_shape_index].name)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	player_in_range = true
 
 func _on_Area2D_body_exited(body):
-	if body.name == "Player":
 		player_in_range = false
+		is_open = false
 
 func _process(_delta):
-	if player_in_range and Input.is_action_just_pressed("interact"):
-		toggle_door()
+	if player_in_range and Input.is_action_just_pressed("ui_accept"):
+		open_door()
+		is_open = true
 		print("open!open!open!")
 
-func toggle_door():
-	is_open = !is_open
-	
+func open_door():
 	if is_open:
-		$CollisionShape2D.disabled = true
-		$Sprite2D.frame = 1  # or change texture
+		collision_shape_2d.disabled = true
+		print("change")
+		#$Sprite2D.frame = 1  # or change texture
 	else:
-		$CollisionShape2D.disabled = false
-		$Sprite2D.frame = 0
+		collision_shape_2d.disabled = false
+		#$Sprite2D.frame = 0
 
 
 
--- scripts/evil_integral.gd
+++ scripts/evil_integral.gd
@@ -2,27 +2,59 @@ extends AnimatableBody2D
 
 var random_int: int = 0
 
-@onready var collision_shape_2d: CollisionShape2D = $killzone/CollisionShape2D
+#@onready var collision_shape_2d: CollisionShape2D = $killzone/CollisionShape2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var collision_polygon_2d: CollisionPolygon2D = $killzone/CollisionPolygon2D
+@onready var collision_polygon_2d_2: CollisionPolygon2D = $killzone/CollisionPolygon2D2
+@onready var timer: Timer = $Timer
+@onready var timer_2: Timer = $Timer2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize() # Initialize seed
-	random_int = randi_range(2, 2) # 1 to 100
+	random_int = randi_range(1, 2) # 1 to 100
 	print(random_int)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	if random_int == 1:
 		animated_sprite_2d.play("y=x")
-		collision_shape_2d.set_deferred("disabled", false)
 		collision_polygon_2d.set_deferred("disabled", true)
 	elif random_int == 2:
 		animated_sprite_2d.play("y=x^2")
-		collision_shape_2d.set_deferred("disabled", true)
-		collision_polygon_2d.set_deferred("disabled", false)
+		collision_polygon_2d_2.set_deferred("disabled", true)
 	else:
 		animated_sprite_2d.play("default")
-		collision_shape_2d.set_deferred("disabled", true)
+		collision_polygon_2d_2.set_deferred("disabled", true)
 		collision_polygon_2d.set_deferred("disabled", true)
+	timer.start()
+	
+func _on_timer_timeout() -> void:
+	if random_int == 1:
+		collision_polygon_2d_2.set_deferred("disabled", false)
+	elif random_int == 2:
+		collision_polygon_2d.set_deferred("disabled", false)
+	timer_2.start()
+
+func _on_timer_2_timeout() -> void:
+	collision_polygon_2d_2.set_deferred("disabled", true)
+	collision_polygon_2d.set_deferred("disabled", true)
+	animated_sprite_2d.play("default")
+
+#func _on_animated_sprite_2d_animation_finished() -> void:
+	#animated_sprite_2d.play("default")
+	#print("is ts even on")
+	#collision_shape_2d.set_deferred("disabled", true)
+	#collision_polygon_2d.set_deferred("disabled", true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#if random_int == 1:
+		#animated_sprite_2d.play("y=x")
+		#collision_polygon_2d.set_deferred("disabled", true)
+	#elif random_int == 2:
+		#animated_sprite_2d.play("y=x^2")
+		#collision_shape_2d.set_deferred("disabled", true)
+	#else:
+		#animated_sprite_2d.play("default")
+		#collision_shape_2d.set_deferred("disabled", true)
+		#collision_polygon_2d.set_deferred("disabled", true)
+	#timer.start()
